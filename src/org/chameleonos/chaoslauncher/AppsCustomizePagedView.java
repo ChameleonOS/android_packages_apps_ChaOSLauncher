3651ae39dc3e135dbacda9e746b3dd1a597f19cf
0xD34D <clark@scheffsblend.com>
2013-04-07 12:44:48 -0400

    If the user cancels adding a widget from the app drawer, the PendingAddWidget should be cleaned up so it does not remain on screen.

--- src/org/chameleonos/chaoslauncher/AppsCustomizePagedView.java
+++ src/org/chameleonos/chaoslauncher/AppsCustomizePagedView.java
@@ -1078,7 +1078,8 @@ public class AppsCustomizePagedView extends PagedViewWithDraggableItems implemen
 
             d.deferDragViewCleanupPostAnimation = false;
         }
-        cleanupWidgetPreloading(success);
+        // should pass false to cleanupWidgetPreloading if drop target is an instance of DeleteDropTarget
+        cleanupWidgetPreloading(success && !(target instanceof DeleteDropTarget));
         mDraggingWidget = false;
     }
 
